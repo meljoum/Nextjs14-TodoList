37b3b6e53998940a284cb275029d9fa993cc0632
Meljoum <elmehdi.meljoum@flyer.be>
2023-12-20 15:31:41 +0100

    Fix links

--- app/page.tsx
+++ app/page.tsx
@@ -21,8 +21,8 @@ function Home() {
         />
       </div>
       <Header text="Todo List" name="" />
-      <Link className="link link-success" href="/todo2">Todo list (Add, Delete && Update) with &quot;React Query&quot; method</Link>
-      <Link className="link link-info" href="/todo-react-query">Todo list (Add, Delete && Update) with simple method</Link>
+      <Link className="link link-success" href="/todo-react-query">Todo list (Add, Delete && Update) with &quot;React Query&quot; method</Link>
+      <Link className="link link-info" href="/todo">Todo list (Add, Delete && Update) with simple method</Link>
     </main>
   );
 }
