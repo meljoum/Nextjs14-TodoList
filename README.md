dbea5abcb125d74fec1767a8d20545a1e49b2747
Meljoum <elmehdi.meljoum@flyer.be>
2023-12-20 10:45:57 +0100

    Change readme file && And fix page link for todo list

--- README.md
+++ README.md
@@ -2,7 +2,19 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 ## Getting Started
 
-First, run the development server:
+First, Install the project :
+
+```bash
+npm install
+```
+
+Run Json Server:
+
+```bash
+npm run json-server
+```
+
+Run the development server:
 
 ```bash
 npm run dev
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 // import { Header } from '@/components/Header'; //* we use Header between braclets {} when we have "export function Header(){}" without export defalut.
-import Counter from '@/components/Counter';
+//import Counter from '@/components/Counter';
 import Header from '@/components/Header';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -8,9 +8,9 @@ import Link from 'next/link';
 
 function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-around p-24">
       
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
@@ -20,11 +20,10 @@ function Home() {
           priority
         />
       </div>
-
-      <p>Hello world</p>
-      <Header text="Header of page" name="Micheal" />
-      <Counter />
-      <Link className="btn_custom" href="/about">Go to About</Link>
+      <Header text="TODO LIST" name="" />
+      {/* <Counter /> */}
+      <Link className="link link-success" href="/todo2">Todo list (Add, Delete && update) with "React Query" methode</Link>
+      <Link className="link link-info" href="/todo-react-query">Todo list (Add, Delete && update) with simple methode</Link>
     </main>
   );
 }
